66690262fea733cacff1ab9954fdef3ce3dfa754
paskal bruh <khadkapaskal2@gmail.com>
2024-07-23 07:53:54 +0545

    first commit

---
+++ src/Components/Assets/data.js
@@ -0,0 +1,34 @@
+import p1_img from './product_1.webp'
+import p2_img from './product_2.webp'
+import p3_img from './product_3.webp'
+import p4_img from './product_4.webp'
+
+let data_product =[
+    {
+        id:1,
+        name:"T-shirt og hiii hyyhyuo joik",
+        image:p1_img,
+        new_price:50.00,
+        old_price:80.50,
+    },
+    {id:2,
+        name:"T-shirt og hiii hyyhyuo joik",
+        image:p2_img,
+        new_price:85.00,
+        old_price:120.50,
+    },
+    {id:3,
+        name:"T-shirt og hiii hyyhyuo joik",
+        image:p3_img,
+        new_price:60.00,
+        old_price:100.50,
+    },
+    {id:4,
+        name:"T-shirt og hiii hyyhyuo joik",
+        image:p4_img,
+        new_price:100.00,
+        old_price:150.00,
+    },
+];
+
+export default data_product;
